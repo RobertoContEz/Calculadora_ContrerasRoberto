0aaaf33f585056fae4a6b40988117d2fd9c5ea34
RobertoContEz <r.c0ntr3ras@gmail.com>
2025-01-30 22:27:55 -0700

    Avance en clase

---
+++ app/src/main/java/contreras/roberto/calculadora/MainActivity.kt
@@ -0,0 +1,48 @@
+package contreras.roberto.calculadora
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val b1: Button = findViewById(R.id.b1)
+        val b2: Button = findViewById(R.id.b2)
+        val b3: Button = findViewById(R.id.b3)
+        val b4: Button = findViewById(R.id.b4)
+        val b5: Button = findViewById(R.id.b5)
+        val b6: Button = findViewById(R.id.b6)
+        val b7: Button = findViewById(R.id.b7)
+        val b8: Button = findViewById(R.id.b8)
+        val b9: Button = findViewById(R.id.b9)
+        val result: TextView = findViewById(R.id.result)
+        val minus: Button = findViewById(R.id.minus)
+        val plus: Button = findViewById(R.id.plus)
+        val equals: Button = findViewById(R.id.equals)
+
+        var n1: Int = 0
+        var n2: Int? = null
+        var op: Char? = null
+
+        
+
+
+
+
+
+
+    }
+}
